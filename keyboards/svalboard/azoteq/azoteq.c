345dc86ccc4cbedcbe87197f27b0e1b08f685169
Ira Cooper <ira@wakeful.net>
2025-12-07 21:49:28 -0500

    azoteq: Slow down the reporting rate on Azoteq.
    There's some minor complications with making sure that
    the held buttons are reported correctly, but other than
    that the code keeps azoteq touchpads at under 100hz.
    
    Minor fix to ci-cd matching upstream.
    
    Fix to the release code for CI/CD.

--- keyboards/svalboard/azoteq/azoteq.c
+++ keyboards/svalboard/azoteq/azoteq.c
@@ -2,13 +2,24 @@
 #include "pointing_device.h"
 #include "pointing_device_internal.h"
 #include "axis_scale.h"
+#include "svalboard.h"
+
+// Azo can only report at 100hz.  Any faster is trouble.
+#define AZO_MS 10 
 
 extern const pointing_device_driver_t *real_device_driver;
 
+static uint16_t azo_timer = 0;
+static uint8_t azo_held_buttons = 0;
 
 report_mouse_t pointing_device_driver_get_report(report_mouse_t mouse_report) {
+    if (timer_elapsed(azo_timer) > AZO_MS) {
+        azo_timer = timer_read();
+        mouse_report        = real_device_driver->get_report(mouse_report);
+        azo_held_buttons = mouse_report.buttons;
+    }
 
-    mouse_report = real_device_driver->get_report(mouse_report);
+    mouse_report.buttons = azo_held_buttons;
 
     return mouse_report;
 }
